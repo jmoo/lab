508bfbc4857df66f3a0e8eacdca82fef370af203
John Moore <me@jmoo.io>
2025-01-11 22:01:01 -0500

    prog

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -14,6 +14,23 @@ with lib;
 
     programs.ghostty = {
       enable = true;
+
+      settings = {
+        theme = "dark:Bright Lights,light:GruvboxLightHard";
+        background-opacity = 0.9;
+        font-family = "UbuntuMono Nerd Font Mono";
+        font-size = 14;
+        window-decoration = false;
+        gtk-tabs-location = "bottom";
+        keybind = [
+          "ctrl+left=previous_tab"
+          "ctrl+right=next_tab"
+          "ctrl+t=new_tab"
+          # "ctrl+shift+t=toggle_tab_overview"
+          # "ctrl+plus=increase_font_size"
+          # "ctrl+minus=decrease_font_size"
+        ];
+      };
     };
   };
 }
