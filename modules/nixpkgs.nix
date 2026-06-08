c1dbbd7761b1cfe197d204f99a9e64e3de2054f7
John Moore <me@jmoo.io>
2026-06-08 19:29:32 -0400

    nixpkgs + treefmt modules

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,40 +1,61 @@
-{ lib', inputs, ... }:
+{
+  lib',
+  config,
+  inputs,
+  ...
+}:
 let
-  inherit (lib'.lab) mkHostModule;
-
-  overlay = import ../overlay.nix inputs;
-
-  # allowUnfree + the global overlay on every platform.
-  common = {
-    nixpkgs = {
-      config.allowUnfree = true;
-      overlays = [ overlay ];
-    };
-  };
-
-  # Linux-only insecure-package allowances.
-  linux = {
-    nixpkgs.config.permittedInsecurePackages = [
-      "libsoup-2.74.3"
-    ];
-  };
+  inherit (lib'.lab) mkHostModule forAll;
+  inherit (lib') types mkOption;
 in
 {
-  options.lab.hosts = mkHostModule (_: {
-    config = {
-      nixos.module = {
-        imports = [
-          common
-          linux
-        ];
+
+  options = {
+    lab = {
+      nixpkgs = {
+        overlays = mkOption {
+          type = types.listOf types.raw;
+          default = [ ];
+        };
+
+        config = mkOption {
+          type = types.attrsOf types.anything;
+          default = { };
+        };
       };
-      asahi.module = {
-        imports = [
-          common
-          linux
-        ];
-      };
-      darwin.module = common;
+
+      hosts = mkHostModule (forAll {
+        inherit (config) nixpkgs;
+      });
     };
-  });
+  };
+
+  config = {
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "libsoup-2.74.3"
+        ];
+      };
+      overlays = [ (import ./overlay.nix inputs) ];
+    };
+
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    perSystem =
+      { system, pkgs, ... }:
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          inherit (config) nixpkgs;
+        };
+
+        legacyPackages = pkgs;
+      };
+  };
 }
